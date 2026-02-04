6998a81caac5bece5aaad6d6d2c8aa4226ffc775
Lokesh1044 <lokeshvempada9533@gmail.com>
2026-02-04 19:47:14 +0530

    Auto deploy test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("🚀 DevOps CI/CD Project Running Successfully!");
+  res.send("🚀 DevOps CI/CD Project Running Successfully! & modified");
 });
 
 const PORT = process.env.PORT || 3000;
