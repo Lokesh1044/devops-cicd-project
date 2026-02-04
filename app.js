74ee0e627922970fa13d25b5452be1436de7f19d
Lokesh1044 <lokeshvempada9533@gmail.com>
2026-02-04 19:21:00 +0530

    Initial DevOps CI/CD project

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("🚀 DevOps CI/CD Project Running Successfully!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
